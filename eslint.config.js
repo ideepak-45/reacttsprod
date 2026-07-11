6c29ffaa9e3fbc17d2e797b72ba6403161dbe6af
ideepak-45 <rajveerdeepak45@gmail.com>
2026-07-12 01:10:01 +0530

    refactor: eslint configurations

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'build']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -18,5 +18,15 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+      "no-console": "error"
+    }
   },
 ])
