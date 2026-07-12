6e91ddf529a5703dbb8a100d30117650df5f8b7e
ideepak-45 <rajveerdeepak45@gmail.com>
2026-07-12 15:46:25 +0530

    feat: environment validator setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type ServerOptions } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
+type TMode = "development" | "production";
+
+interface AppEnv {
+    VITE_ENV: TMode;
+    PORT: string;
+}
+
+const validateEnv = (envMode: TMode, env: AppEnv) => {
+    const requiredVariables: (keyof AppEnv)[] = ["PORT", "VITE_ENV"];
+
+    for (const key of requiredVariables) {
+        if (!env[key]) {
+            throw new Error(`${key} is missing! Please define it in your .env.${envMode}`);
+        }
+    }
+};
+
+const normalizePort = (port: string) => {
+    const normalizedPort = parseInt(port);
+    if (isNaN(normalizedPort)) {
+        throw new Error(`Invalid Port value: ${port}`);
+    }
+    return normalizedPort;
+};
+
+export default defineConfig(({ mode }) => {
+    const envMode = mode as TMode;
+    const env = loadEnv(envMode, process.cwd(), "") as unknown as AppEnv;
+    validateEnv(envMode, env);
+
+    const port = normalizePort(env.PORT);
+
+    const config: ServerOptions = {
+        port,
+        open: true,
+    };
+
+    return {
+        plugins: [react(), tailwindcss()],
+        server: config,
+        preview: config,
+        build: {
+            minify: true,
+        },
+    };
 });
